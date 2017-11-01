65d8d0eba19fecbf09ba7ee991772cc9568c1b6d
Nick DeGroot <nbdegroot1@gmail.com>
2017-11-01 10:50:22 -0700

    Removed unnecessary files & set up outline

---
+++ App/Containers/HomeScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, ScrollView, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+export default class HomeScreen extends Component {
+  render () {
+    return (
+      <View>
+        <ScrollView>
+          <Text>TODO make homescreen</Text>
+        </ScrollView>
+      </View>
+    )
+  }
+}
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,36 +1,48 @@
 import React from 'react'
 import { NavigationComponent } from 'react-native-material-bottom-navigation'
-import { TabNavigator } from 'react-navigation'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import LaunchScreen from '../Containers/LaunchScreen'
-import TestScreen from '../Containers/TestScreen'
+import SignInScreen from '../Containers/SignInScreen'
+
+import HomeScreen from '../Containers/HomeScreen'
+import RequestScreen from '../Containers/RequestScreen'
 
 // Manifest of possible screens
-const PrimaryNav = TabNavigator({
-  LaunchScreen: { screen: LaunchScreen },
-  TestScreen: { screen: TestScreen }
+const TabNav = TabNavigator({
+  HomeScreen: { screen: HomeScreen },
+  RequestScreen: { screen: RequestScreen }
 }, {
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'HomeScreen',
+  swipeEnabled: true,
+  animationEnabled: true,
   tabBarComponent: NavigationComponent,
   tabBarPosition: 'bottom',
   tabBarOptions: {
     bottomNavigationOptions: {
       activeLabelColor: '#03a9f4',
       tabs: {
-        LaunchScreen: {
+        HomeScreen: {
           activeIcon: <Icon size={24} color='#03a9f4' name='home' />,
           icon: <Icon size={24} color='black' name='home' />,
           label: 'Home'
         },
-        TestScreen: {
-          activeIcon: <Icon size={24} color='#03a9f4' name='mood' />,
-          icon: <Icon size={24} color='black' name='mood' />,
-          label: 'Test'
+        RequestScreen: {
+          activeIcon: <Icon size={24} color='#03a9f4' name='swap-horiz' />,
+          icon: <Icon size={24} color='black' name='swap-horiz' />,
+          label: 'Request'
         }
       }
     }
   }
+})
+
+const PrimaryNav = StackNavigator({
+  SignInScreen: { screen: SignInScreen },
+  TabNav: { screen: TabNav }
+}, {
+  initialRouteName: 'TabNav',
+  headerMode: 'none'
 })
 
 export default PrimaryNav
