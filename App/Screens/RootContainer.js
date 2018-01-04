75b7037a91673e9248bee2c5377b15c04bfff4f8
Nick DeGroot <nbdegroot1@gmail.com>
2018-01-03 19:24:57 -0800

    Cleaned up small linting problem

--- App/Screens/RootContainer.js
+++ App/Screens/RootContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StatusBar, Platform } from 'react-native'
+import { View, StatusBar } from 'react-native'
 import ReduxNavigation from '../Navigation/ReduxNavigation'
 import { connect } from 'react-redux'
 import StartupActions from '../Redux/StartupRedux'
