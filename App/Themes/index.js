53cdadbcd4ff679745972a87e6abfedb048e2ab9
Nick DeGroot <nbdegroot1@gmail.com>
2018-08-21 03:35:16 -0700

    Remove old native files

--- App/Themes/index.js
+++
@@ -1,6 +0,0 @@
-import Colors from './Colors'
-import Fonts from './Fonts'
-import Metrics from './Metrics'
-import ApplicationStyles from './ApplicationStyles'
-
-export { Colors, Fonts, Metrics, ApplicationStyles }
--- index.js
+++
@@ -1,5 +0,0 @@
-import './App/Config/ReactotronConfig'
-import { AppRegistry } from 'react-native'
-import App from './App/Screens/App'
-
-AppRegistry.registerComponent('Homeroom', () => App)
